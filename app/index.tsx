4c6427523295c0123ebee2eee212c9d2d7d671e7
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-02-23 18:07:05 -0600

    refactor: cleaning project; deleted unusing components and configuration by default

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>ESTA ES MI CALCULADORA ALV</Text>
+    </View>
+  )
+}
+
+export default CalculatorApp
