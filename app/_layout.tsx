b9ea1d6e89372f5b198d82857888fcff4be4a235
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-02-23 18:28:14 -0600

    feature: add constant color; global styles and import fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { Colors } from '@/constants/Colors'
+import { StatusBar } from 'expo-status-bar'
 
-const RootLayout = () => {
+const RootLayout = () => {  
+
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    })
+
+    if (!loaded) {
+        return null
+    }
+
+
   return (
-    <View>
+    <View style={{backgroundColor: Colors.background, flex: 1}}>
       <Text>_layout</Text>
         <Slot/>
+
+
+     <StatusBar style="dark" />
     </View>
   )
 }
 
 export default RootLayout
+
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Colors } from '@/constants/Colors'
 
 const CalculatorApp = () => {
   return (
     <View>
-      <Text>ESTA ES MI CALCULADORA ALV</Text>
+      <Text style={{fontSize: 
+        20, textAlign: 'center', marginTop
+        : 20, color: Colors.textPrimary,
+        fontFamily: 'SpaceMono'
+      }}>ESTA ES MI CALCULADORA</Text>
     </View>
   )
 }
